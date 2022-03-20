5df33b85a98a751c9bf3a566d077b58b80eeb110
Maia Kelly <maiakelly11@gmail.com>
2022-03-20 15:58:02 -0400

    update

--- init.R
+++ init.R
@@ -1,10 +1,1 @@
-
-my_packages = c("leaflet", "DT", "spData", "dplyr", "maps", "googlesheets4", "sf")
-
-install_if_missing = function(p) {
-  if (p %in% rownames(installed.packages()) == FALSE) {
-    install.packages(p)
-  }
-}
-
-invisible(sapply(my_packages, install_if_missing))
+helpers.installPackages("leaflet", "DT", "spData", "dplyr", "maps", "googlesheets4", "sf")
